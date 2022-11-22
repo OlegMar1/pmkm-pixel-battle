872188a2a4526a0ca2c231df90a1e89a05300999
YuriiMoroz <yurii.moroz.pm.2018@lpnu.ua>
2022-11-23 00:56:15 +0200

    Two hours later...

--- frontend/angular/src/app/main/main.component.ts
+++ frontend/angular/src/app/main/main.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { generate } from 'rxjs';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  selector: 'app-footter',
+  templateUrl: './footter.component.html',
+  styleUrls: ['./footter.component.scss']
 })
-export class MainComponent {
+export class FootterComponent implements OnInit{
+
+  public fakeArr = new Array(8450)
+  public colors = new Array(10)
+  public color!: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    let someArr = document.getElementsByClassName('colors');
+    console.log(someArr)
+    // for (let i = 0; i < someArr.length; i++) {
+    //   console.log('elem i')
+    //   console.log(someArr[i])  
+    // }
+  }
+  public setColor(event:Event):void{
+    const elem:HTMLDivElement=<HTMLDivElement>event.target
+    elem.style.backgroundColor='red'
+  }
+  public SaveColor(event:Event):void{
+    const elem:HTMLDivElement=<HTMLDivElement>event.target
+    this.color = elem.style.backgroundColor
+    console.log(this.color)
+  }
 
 }
