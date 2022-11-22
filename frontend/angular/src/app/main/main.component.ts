83f67ffd6faea03eb21c912f7722fbcaad5c8d5c
OlegMar1 <mixassv11@gmail.com>
2022-11-23 01:06:24 +0200

    fix

--- frontend/angular/src/app/main/main.component.ts
+++ frontend/angular/src/app/main/main.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { generate } from 'rxjs';
 
 @Component({
-  selector: 'app-footter',
-  templateUrl: './footter.component.html',
-  styleUrls: ['./footter.component.scss']
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
 })
-export class FootterComponent implements OnInit{
+export class MainComponent implements OnInit{
 
   public fakeArr = new Array(8450)
   public colors = new Array(10)
@@ -19,7 +19,7 @@ export class FootterComponent implements OnInit{
     console.log(someArr)
     // for (let i = 0; i < someArr.length; i++) {
     //   console.log('elem i')
-    //   console.log(someArr[i])  
+    //   console.log(someArr[i])
     // }
   }
   public setColor(event:Event):void{
