82919bed0296101d1b12e998ab808abb5c2ce9c8
YuriiMoroz <yurii.moroz.pm.2018@lpnu.ua>
2023-01-26 17:06:29 +0200

    Paint2

--- frontend/angular/src/app/main/main.component.ts
+++ frontend/angular/src/app/main/main.component.ts
@@ -16,22 +16,40 @@ export class MainComponent implements OnInit{
   private colorBlock!:HTMLDivElement
   private isCell = false;
   private isColor = false;
+  
+
 
   constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
 //     Pusher.LogToConsole = true;
-//
+
 //     const pusher = new Pusher('25291c0752d6089a660d', {
 //       cluster: 'eu'
 //     });
-//
+
 //     const channel = pusher.subscribe('pixel-battle-channel');
 //      chanel.bind('pixel-data', data => {
 // //         parse data
 // //         color = data['color'];
 //     });
+    Pusher.logToConsole = true;
+
+    var pusher = new Pusher('eaf74954e926bfb7e254', {
+      cluster: 'eu'
+    });
+
+    var channel = pusher.subscribe('pixel-battle-channel');
+    channel.bind('paint-pixel', function(data) {
+      alert(JSON.stringify(data));
+      for (let i = 0; i < data.length; i++) {
+        let x =parseInt(data[i].coord_x);
+        let y =parseInt(data[i].coord_y);
+        let color = data[i].color
+        this.Paint2(x,y,color)
+      }
+    });
   }
 
   public generateIndex(){
@@ -58,7 +76,7 @@ export class MainComponent implements OnInit{
 
   public SaveColor(event:Event):void{
     if(!(this.colorBlock==undefined)){
-      this.colorBlock.style.border = '1px solid gray';
+      this.colorBlock.style.border = '1px solid black';
     }
     this.colorBlock=<HTMLDivElement>event.target
     this.colorBlock.style.border = '2px solid rgb(80, 80, 197)'
@@ -73,14 +91,30 @@ export class MainComponent implements OnInit{
     if(this.isCell&&this.isColor){
       this.cell.style.backgroundColor=this.color;
       this.cell.style.border = 'none';
-      this.colorBlock.style.border = '1px solid gray';
+      let coords = this.cell.id.split(':')
       this.cell = <HTMLDivElement> document.querySelector('.anreal');
       this.isCell=false;
-      this.isColor=false;
       this.http.post('http://127.0.0.1:5000/paint-pixel', {
         test: 'test-message2',
+        coord_x: parseInt(coords[0]),
+        coord_y: parseInt(coords[1]),
         color: this.color
       }).subscribe();
     }
   }
+
+  public Paint2(x:number,y:number,color:string):void{
+    this.cell = (<HTMLDivElement>document.getElementById(x+':'+y))
+    this.cell.style.backgroundColor=color
+    this.cell.style.border = 'none';
+  }
+
+
+  test():void{
+    let str='1'
+    console.log(str)
+  }
+
 }
+
+//npm/yarn run ngcc
